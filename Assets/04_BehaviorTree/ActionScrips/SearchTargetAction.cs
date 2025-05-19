97ecc2968e826d44841879d81aead284c8ebca42
clanbert13 <clanbert13@gmail.com>
2025-05-19 19:47:31 +0900

    add enemy BT
    prepareing for make an Ai for enemy

---
+++ Assets/04_BehaviorTree/ActionScrips/SearchTargetAction.cs
@@ -0,0 +1,28 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "SearchTarget", story: "[Self] Searching for [Target]", category: "Action", id: "dfddb7c3a557330d11ea260f616d3dcf")]
+public partial class SearchTargetAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
