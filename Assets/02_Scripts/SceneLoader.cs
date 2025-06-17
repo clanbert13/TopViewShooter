9774c23d78345eb244518e569588b1d80703290e
clanbert13 <clanbert13@gmail.com>
2025-06-18 07:35:09 +0900

    real final

--- Assets/02_Scripts/SceneLoader.cs
+++ Assets/02_Scripts/SceneLoader.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // 씬 관리 기능을 사용하기 위해 추가
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad = "GamePlayScene"; // 로드할 씬의 이름 (Inspector에서 설정)
 
-    // 이 함수는 UI 버튼의 OnClick() 이벤트에 연결할 때 사용됩니다.
+    // 이 함수는 버튼의 OnClick() 이벤트에 연결
     public void LoadGameScene()
     {
         Time.timeScale = 1f; 
@@ -20,7 +20,7 @@ public class SceneLoader : MonoBehaviour
     {
         Debug.Log("Quitting game...");
         Application.Quit(); // 게임 종료
-        // 에디터에서는 Application.Quit()이 동작하지 않으므로 아래 코드를 추가하여 확인
+        // 에디터에서 Application.Quit() 동작 확인
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
